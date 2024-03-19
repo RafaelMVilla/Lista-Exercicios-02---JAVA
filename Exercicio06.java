a8ec40cf6a1b657968f149cd48d149018ae3c156
RafaelMVilla <rafael.march.v@gmail.com>
2024-03-19 18:02:54 -0300

    Exercicio06

--- Exercicio06.java
+++ Exercicio06.java
@@ -6,6 +6,8 @@ Elabore um programa que leia um vetor com 5 notas de um aluno e um outro vetor c
 Média ponderada =  (nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)
                    ---------------------------------------------------
                                 peso1 + peso2 + peso3
+
+Ex.: notas: 7.5, 9.2 pesos: 6, 4. Média ponderada: (7.5 * 6 + 9.2 * 4) / (6 + 4)
  */
 
 public class Exercicio06 {
