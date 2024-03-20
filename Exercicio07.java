02ccc646e39b43c1afee7e4a016953d8aedc52de
RafaelMVilla <rafael.march.v@gmail.com>
2024-03-20 08:31:43 -0300

    Exercicio07

--- Exercicio07.java
+++ Exercicio07.java
@@ -1,8 +1,35 @@
 
 /*
-
+Faça um programa que leia dois vetores de 5 elementos cada e verifique se eles são iguais ou não. Para serem iguais, todos os elementos dos dois vetores devem coincidir.
  */
 
 public class Exercicio07 {
     
+    public static void executar(){
+
+        int contador = 5;
+        String[] elemento1 = new String[contador];
+        String[] elemento2 = new String[contador];
+
+        for (int i = 0; i < contador; i++) {
+            
+            elemento1[i] = Prompt.lerLinha("Digite seu primeiro elemento para comparação: ");
+            elemento2[i] = Prompt.lerLinha("Digite seu segundo elemento para comparação: ");
+            Prompt.imprimir("----------------------------------------"); 
+
+        }
+
+        for (int i = 0; i < contador; i++) {
+            
+            if (elemento1[i].equals(elemento2[i])) { 
+
+                Prompt.imprimir("Os elementos: " + elemento1[i] + " e " + elemento2[i] + ": São iguais.");
+                Prompt.imprimir("----------------------------------------");  
+
+            }else {
+                Prompt.imprimir("Os elementos: " + elemento1[i] + " e " + elemento2[i] + ": São diferentes.");
+                Prompt.imprimir("----------------------------------------"); 
+            } 
+        }         
+    }
 }
