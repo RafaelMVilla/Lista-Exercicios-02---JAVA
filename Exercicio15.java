3db0b54c4b5c2d3330b7c2047ce0b4fcbaf4d7ba
RafaelMVilla <rafael.march.v@gmail.com>
2024-03-23 19:05:04 -0300

    Exercicio15

--- Exercicio15.java
+++ Exercicio15.java
@@ -6,6 +6,33 @@ Elabore um algoritmo que leia 5 números do teclado e preencha um vetor de acord
 public class Exercicio15 {
     
     public static void executar(){
+
+        int Numdigitado = 1;
+        int[] Num = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            
+            while (true) {
+                Num[i] = Prompt.lerInteiro("Digite um número: ");
+
+                if (Num[i] > Numdigitado) {
+                    Numdigitado = Num[i];
+                    break;
+                } else {
+                    Prompt.imprimir("Erro! Digite Novamente: ");
+                }
+            }
+
+        }
+
+        Prompt.imprimir("--------------");
+        Prompt.imprimir("Valores: ");
         
+
+        for (int i = 0; i < 5; i++) {
+        
+            Prompt.imprimir(Num[i]);
+        }
     }
 }
+
